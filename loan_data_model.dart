81b9c46dad254330eeed2827c81ff01780c378cc
Jasmine Sathupati <sathupatihepsiba@karunya.edu.in>
2025-11-07 02:23:54 +0530

    Create loan_data_model.dart

---
+++ loan_data_model.dart
@@ -0,0 +1,15 @@
+class LoanData {
+  final String name;
+  final int age;
+  final double salary;
+  final double existingEmi;
+  final double loanAmount;
+
+  LoanData({
+    required this.name,
+    required this.age,
+    required this.salary,
+    required this.existingEmi,
+    required this.loanAmount,
+  });
+}
